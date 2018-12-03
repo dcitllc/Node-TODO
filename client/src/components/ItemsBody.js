9f003e3bbe2109f5269dd994a48d5e655bd8d4ca
Daniel Chamorro <dan@dcitllc.com>
2018-12-02 22:17:38 -0500

    Getting TODOs - Added axios server side - Added http-proxy-middleware to handle proxy - setupProxy: config file for proxy middleware - Map over ListItem to render list in ItemsBody

--- client/src/components/ItemsBody.js
+++ client/src/components/ItemsBody.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
 import ListItems from "./ListItems";
 import ListItem from "./ListItem";
+import axios from "axios";
 
 class ItemsBody extends Component {
+  state = {
+    loading: true,
+    todos: {}
+  };
+
+  componentDidMount() {
+    axios.get("api/todo").then(response => {
+      this.setState({
+        loading: false,
+        todos: response.data
+      });
+    });
+  }
+
   render() {
+    if (this.state.loading === true) {
+      return <h1>Loading...</h1>;
+    }
+
     return (
       <ListItems>
-        <ListItem text="Hello" />
+        {this.state.todos.map(todo => {
+          return (
+            <ListItem title={todo.title} isDone={todo.is_done} key={todo.id} />
+          );
+        })}
       </ListItems>
     );
   }
